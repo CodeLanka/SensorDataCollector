07556f0199aeb9f45907f41309aba035cdaad9a1
lakmalniranga <lakmalniranga@gmail.com>
2018-10-15 01:56:56 +0530

    Add redux to handle app state. Create label selector UI

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 // import IdentificationScreen from './src/screens/IdentificationScreen';
 import HomeScreen from './src/screens/HomeScreen';
+
+import rootReducer from './src/reducers';
+
+const store = createStore(rootReducer);
 
 export default class App extends React.Component {
     render() {
         return (
-            <HomeScreen />
+            <Provider store={store}>
+                <HomeScreen />
+            </Provider>
         );
     }
 }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -8,20 +8,149 @@ import {
     Picker,
     Button,
 } from 'react-native';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import SensorIcon from '../component/SensorIcon';
 import ListItem from '../component/ListItem';
 import Divider from '../component/Divider';
 
+import {
+    openSelectorAction,
+    closeSelectorAction,
+    levelOneChangeAction,
+    levelTwoChangeAction,
+    levelThreeChangeAction,
+    levelFourChangeAction,
+} from '../actions/selectorActions';
+
 // TODO: Break divider into a component
-export default class HomeScreen extends React.Component {
+class HomeScreen extends React.Component {
     state = {
-        modalVisible: false,
-        levelOne: null,
+        levelOneData: [
+            { value: null, label: 'Please select transpotation method' },
+            { value: 'TRAIN', label: 'Train' },
+            { value: 'BUS', label: 'Bus' },
+            { value: 'WALKING', label: 'Walking' },
+            { value: 'PERSONAL', label: 'Car/Personal vehicle' },
+            { value: 'BIKE', label: 'Bike' },
+            { value: 'BICYCLE', label: 'Bicycle' },
+            { value: 'OTHER', label: 'Other' },
+        ],
+        levelTwoData: {
+            TRAIN: [
+                { value: null, label: 'Please select train type' },
+                { value: 'EXPRESS', label: 'Express' },
+                { value: 'SLOW', label: 'Slow' },
+                { value: 'INTERCITY', label: 'Intercity' },
+            ],
+            ROAD: [
+                { value: null, label: 'Please select road type' },
+                { value: 'HIGHWAY', label: 'Highway' },
+                { value: 'CARPET', label: 'Carpet road' },
+                { value: 'LOW_MAINTAINANCE', label: 'Road with low maintainance' },
+                { value: 'GRAVEL', label: 'Gravel road' },
+                { value: 'PATH', label: 'Path' },
+            ],
+        },
+        levelThreeData: {
+            POSSITION: [
+                { value: null, label: 'Please select possition type' },
+                { value: 'SITTING', label: 'Sitting' },
+                { value: 'STANDING', label: 'Standing' },
+            ],
+            CONDITION: [
+                { value: null, label: 'Please select condition type' },
+                { value: 'BRANDNEW', label: 'Brandnew' },
+                { value: 'OLD', label: 'Old(10+ years)' },
+            ],
+            ROLE: [
+                { value: null, label: 'Please select role' },
+                { value: 'RIDER', label: 'rider' },
+                { value: 'PASSENGER', label: 'passenger' },
+            ],
+        },
+        levelFourData: {
+            CLASS: [
+                { value: null, label: 'Please select class' },
+                { value: 'FIRST_CLASS', label: '1st class' },
+                { value: 'SECOND_CLASS', label: '2nd class' },
+                { value: 'THIRD_CLASS', label: '3rd class' },
+            ],
+            TYPE: [
+                { value: null, label: 'Please select type of bus' },
+                { value: 'HIGHWAY', label: 'Highway bus' },
+                { value: 'INTERCITY', label: 'Intercity' },
+                { value: 'SEMI_LUXURY', label: 'Semi luxury' },
+                { value: 'NORMAL', label: 'Normal' },
+                { value: 'DOUBLE_DECKER', label: 'Double decker' },
+                { value: 'ONE_DOOR', label: 'One door bus' },
+            ],
+            MANFATURING_COUNTRY: [
+                { value: null, label: 'Please select manufacturing country' },
+                { value: 'JAPAN', label: 'Japan' },
+                { value: 'INDIA', label: 'India' },
+                { value: 'OTHER', label: 'Other' },
+            ],
+        },
     };
 
-    setModalVisible(visible) {
-        this.setState({ modalVisible: visible });
+    static propTypes = {
+        visibleLabelSelector: PropTypes.bool.isRequired,
+        openSelector: PropTypes.func.isRequired,
+        closeSelector: PropTypes.func.isRequired,
+        levelOneChange: PropTypes.func.isRequired,
+        levelTwoChange: PropTypes.func.isRequired,
+        levelThreeChange: PropTypes.func.isRequired,
+        levelFourChange: PropTypes.func.isRequired,
+        selectedLevelOneType: PropTypes.string,
+        selectedLevelTwoType: PropTypes.string,
+        selectedLevelThreeType: PropTypes.string,
+        selectedLevelFourType: PropTypes.string,
+    }
+
+    levelOneList = () => {
+        const { levelOneData } = this.state;
+        return levelOneData.map(obj => <Picker.Item label={obj.label} value={obj.value} key={obj.value} />);
+    }
+
+    levelTwoList = () => {
+        const { levelTwoData } = this.state;
+        const { selectedLevelOneType } = this.props;
+        if (selectedLevelOneType === 'TRAIN') {
+            return levelTwoData.TRAIN.map(obj => <Picker.Item label={obj.label} value={obj.value} key={obj.value} />);
+        }
+        return levelTwoData.ROAD.map(obj => <Picker.Item label={obj.label} value={obj.value} key={obj.value} />);
+    }
+
+    levelThreeList = () => {
+        const { levelThreeData } = this.state;
+        const { selectedLevelOneType } = this.props;
+        if (selectedLevelOneType === 'TRAIN' || selectedLevelOneType === 'BUS') {
+            return levelThreeData.POSSITION.map(obj => <Picker.Item label={obj.label} value={obj.value} key={obj.value} />);
+        }
+        if (selectedLevelOneType === 'PERSONAL') {
+            return levelThreeData.CONDITION.map(obj => <Picker.Item label={obj.label} value={obj.value} key={obj.value} />);
+        }
+        if (selectedLevelOneType === 'BIKE' || selectedLevelOneType === 'BICYCLE') {
+            return levelThreeData.ROLE.map(obj => <Picker.Item label={obj.label} value={obj.value} key={obj.value} />);
+        }
+        return <Picker.Item label="N/A" value={null} />;
+    }
+
+    levelFourList = () => {
+        const { levelFourData } = this.state;
+        const { selectedLevelOneType } = this.props;
+        if (selectedLevelOneType === 'TRAIN') {
+            return levelFourData.CLASS.map(obj => <Picker.Item label={obj.label} value={obj.value} key={obj.value} />);
+        }
+        if (selectedLevelOneType === 'BUS') {
+            return levelFourData.TYPE.map(obj => <Picker.Item label={obj.label} value={obj.value} key={obj.value} />);
+        }
+        if (selectedLevelOneType === 'PERSONAL') {
+            return levelFourData.MANFATURING_COUNTRY.map(obj => <Picker.Item label={obj.label} value={obj.value} key={obj.value} />);
+        }
+        return <Picker.Item label="N/A" value={null} />;
     }
 
     render() {
@@ -38,13 +167,25 @@ export default class HomeScreen extends React.Component {
             modalContainer,
             modalInnerContainer,
         } = styles;
-        const { modalVisible, levelOne } = this.state;
+        const {
+            visibleLabelSelector,
+            openSelector,
+            closeSelector,
+            levelOneChange,
+            levelTwoChange,
+            levelThreeChange,
+            levelFourChange,
+            selectedLevelOneType,
+            selectedLevelTwoType,
+            selectedLevelThreeType,
+            selectedLevelFourType,
+        } = this.props;
         return (
             <View style={container}>
                 <Modal
                     animationType="slide"
                     transparent
-                    visible={modalVisible}
+                    visible={visibleLabelSelector}
                     onRequestClose={() => { this.setModalVisible(false); }}
                 >
                     <View style={modalContainer}>
@@ -52,31 +193,51 @@ export default class HomeScreen extends React.Component {
                         <View style={modalInnerContainer}>
                             <View style={{ flex: 1 }}>
                                 <Picker
-                                    selectedValue={levelOne}
-                                    onValueChange={itemValue => this.setState({ levelOne: itemValue })}
+                                    selectedValue={selectedLevelOneType}
+                                    onValueChange={itemValue => levelOneChange(itemValue)}
                                     style={{ height: 40 }}
                                 >
-                                    <Picker.Item label="Please select transpotation method" value={null} />
-                                    <Picker.Item label="Train" value="Train" />
-                                    <Picker.Item label="Bus" value="Bus" />
-                                    <Picker.Item label="Walking" value="Walking" />
-                                    <Picker.Item label="Car/Personal vehicle" value="Personal" />
-                                    <Picker.Item label="Bike" value="Bike" />
-                                    <Picker.Item label="Bicycle" value="Bicycle" />
-                                    <Picker.Item label="Other" value="other" />
+                                    {this.levelOneList()}
                                 </Picker>
+                                {selectedLevelOneType != null && (
+                                    <Picker
+                                        selectedValue={selectedLevelTwoType}
+                                        onValueChange={itemValue => levelTwoChange(itemValue)}
+                                        style={{ height: 40 }}
+                                    >
+                                        {this.levelTwoList()}
+                                    </Picker>
+                                )}
+                                {selectedLevelOneType != null && (
+                                    <Picker
+                                        selectedValue={selectedLevelThreeType}
+                                        onValueChange={itemValue => levelThreeChange(itemValue)}
+                                        style={{ height: 40 }}
+                                    >
+                                        {this.levelThreeList()}
+                                    </Picker>
+                                )}
+                                {selectedLevelOneType != null && (
+                                    <Picker
+                                        selectedValue={selectedLevelFourType}
+                                        onValueChange={itemValue => levelFourChange(itemValue)}
+                                        style={{ height: 40 }}
+                                    >
+                                        {this.levelFourList()}
+                                    </Picker>
+                                )}
                             </View>
                             <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
                                 <View style={{ marginLeft: 5 }}>
                                     <Button
-                                        onPress={() => this.setModalVisible(!modalVisible)}
+                                        onPress={() => closeSelector()}
                                         title="CANCEL"
                                         color="#5b5b5b"
                                     />
                                 </View>
                                 <View style={{ marginLeft: 5 }}>
                                     <Button
-                                        onPress={() => this.setModalVisible(!modalVisible)}
+                                        onPress={() => closeSelector()}
                                         title="START"
                                         color="#12806a"
                                     />
@@ -87,7 +248,7 @@ export default class HomeScreen extends React.Component {
                 </Modal>
                 <View style={actionContainer}>
                     <View style={actionButtonContainer}>
-                        <TouchableOpacity style={actionButton} onPress={() => { this.setModalVisible(true); }}>
+                        <TouchableOpacity style={actionButton} onPress={() => { openSelector(); }}>
                             <Text style={actionButtonText}>START</Text>
                         </TouchableOpacity>
                     </View>
@@ -114,6 +275,29 @@ export default class HomeScreen extends React.Component {
         );
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        visibleLabelSelector: state.modals.visibleLabelSelector,
+        selectedLevelOneType: state.lavels.selectedLevelOneType,
+        selectedLevelTwoType: state.lavels.selectedLevelTwoType,
+        selectedLevelThreeType: state.lavels.selectedLevelThreeType,
+        selectedLevelFourType: state.lavels.selectedLevelFourType,
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        openSelector: () => dispatch(openSelectorAction()),
+        closeSelector: () => dispatch(closeSelectorAction()),
+        levelOneChange: payload => dispatch(levelOneChangeAction(payload)),
+        levelTwoChange: payload => dispatch(levelTwoChangeAction(payload)),
+        levelThreeChange: payload => dispatch(levelThreeChangeAction(payload)),
+        levelFourChange: payload => dispatch(levelFourChangeAction(payload)),
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen);
 
 const styles = StyleSheet.create({
     container: {
